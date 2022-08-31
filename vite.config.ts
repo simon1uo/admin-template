0d40c7b7261d09ee74f1ccbc879459d38078fd45
Simon Luo <simonlaw910@gmail.com>
2022-08-31 12:05:05 +0800

    init: init project with basic vite config ✨

---
+++ vite.config.ts
@@ -0,0 +1,39 @@
+import { ConfigEnv, defineConfig, loadEnv } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+import { convertEnv, getRootPath, getSrcPath } from "./build/utils";
+import { createViteProxy } from "./build/config/proxy";
+
+// https://vitejs.dev/config/
+export default defineConfig((configEnv: ConfigEnv) => {
+  const srcPath = getSrcPath();
+  const rootPath = getRootPath();
+
+  const viteEnv = convertEnv(loadEnv(configEnv.mode, process.cwd()));
+
+  const { VITE_PUBLIC_PATH, VITE_PORT, VITE_USE_PROXY, VITE_PROXY_TYPE } =
+    viteEnv;
+  return {
+    base: VITE_PUBLIC_PATH || "/",
+    resolve: {
+      alias: {
+        "~": rootPath,
+        "@": srcPath,
+      },
+    },
+    server: {
+      host: "0.0.0.0",
+      port: VITE_PORT,
+      open: false,
+      proxy: createViteProxy(VITE_USE_PROXY, VITE_PROXY_TYPE as ProxyType),
+    },
+    plugins: [vue()],
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@import '@/styles/variables.scss';`,
+        },
+      },
+    },
+  };
+});
