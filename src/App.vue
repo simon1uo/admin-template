0b15506ce94d0daede76cbe3e017b0e36116ca0e
Simon Luo <simonlaw910@gmail.com>
2022-08-31 20:38:20 +0800

    feat: common ui component config

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <p font-600 text-10>naive</p>
-    <n-space m-50>
+    <p text-10>naive</p>
+    <n-space>
       <n-button>Default</n-button>
       <n-button type="tertiary"> Tertiary </n-button>
       <n-button type="primary"> Primary </n-button>
@@ -10,9 +10,21 @@
       <n-button type="warning"> Warning </n-button>
       <n-button type="error"> Error </n-button>
     </n-space>
+    <AppProvider></AppProvider>
   </div>
 </template>
 
-<script setup></script>
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import AppProvider from '@/components/common/AppProvider.vue'
+
+onMounted(() => {
+  $loadingBar.start()
+  setTimeout(() => {
+    $loadingBar.finish()
+    $message.success('succss~')
+  }, 500)
+})
+</script>
 
 <style lang="scss"></style>
