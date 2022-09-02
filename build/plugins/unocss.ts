07f15eb25345e00e811c0ba09470fb5a670b8715
Simon Luo <simonlaw910@gmail.com>
2022-09-02 17:14:18 +0800

    build(projects): add deploy config

--- build/plugins/unocss.ts
+++ build/plugins/unocss.ts
@@ -1,9 +1,5 @@
 import Unocss from 'unocss/vite'
 
-import { presetUno, presetAttributify, presetIcons } from 'unocss'
-
 export function configUnocss() {
-  return Unocss({
-    presets: [presetUno(), presetAttributify(), presetIcons()],
-  })
+  return Unocss()
 }
