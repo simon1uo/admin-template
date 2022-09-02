b7b732f6615dea602e337e34201fe59435ff93c9
Simon Luo <simonlaw910@gmail.com>
2022-09-02 17:05:11 +0800

    build(projects): add commit message custom tools config

--- mock/api/dashboard.js
+++ mock/api/dashboard.js
@@ -1,27 +1,27 @@
 import { Random } from 'mockjs'
 
 const consoleInfo = {
-  //访问量
+  // 访问量
   visits: {
     dayVisits: Random.float(10000, 99999, 2, 2),
     rise: Random.float(10, 99),
     decline: Random.float(10, 99),
     amount: Random.float(99999, 999999, 3, 5),
   },
-  //销售额
+  // 销售额
   saleroom: {
     weekSaleroom: Random.float(10000, 99999, 2, 2),
     amount: Random.float(99999, 999999, 2, 2),
     degree: Random.float(10, 99),
   },
-  //订单量
+  // 订单量
   orderLarge: {
     weekLarge: Random.float(10000, 99999, 2, 2),
     rise: Random.float(10, 99),
     decline: Random.float(10, 99),
     amount: Random.float(99999, 999999, 2, 2),
   },
-  //成交额度
+  // 成交额度
   volume: {
     weekLarge: Random.float(10000, 99999, 2, 2),
     rise: Random.float(10, 99),
